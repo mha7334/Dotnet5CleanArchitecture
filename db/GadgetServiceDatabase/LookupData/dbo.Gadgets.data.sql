0e242f591b0768677f9d0a5cf2ec5081c61a31ab
Muhammad Asif <muasf@vestas.com>
2021-10-20 10:34:18 +0200

    database project table creation and dummy data

---
+++ db/GadgetServiceDatabase/LookupData/dbo.Gadgets.data.sql
@@ -0,0 +1,27 @@
+﻿SET IDENTITY_INSERT [dbo].[Gadgets] ON
+GO
+
+MERGE INTO [dbo].[Gadgets] AS [Target]
+USING (VALUES
+(1, 'TEST1'),
+(2, 'TEST1'),
+(3, 'TEST1'),
+(4, 'TEST1'),
+(5, 'TEST1'),
+(6, 'TEST1'))
+
+AS [Source] ([Id], [Name])
+ON [Target].[Id] = [Source].[Id]
+WHEN MATCHED THEN --update matched rows
+UPDATE SET
+		 [Name] = [Source].[Name]
+WHEN NOT MATCHED BY TARGET THEN --insert new rows
+INSERT ([Id], [Name])
+VALUES ([Id], [Name]);
+
+-- WHEN NOT MATCHE BY SOURCE THEN -- delete rows that are in the target but not the source
+--DELETE;
+
+GO 
+SET IDENTITY_INSERT [dbo].[Gadgets] OFF
+GO
---
+++ db/GadgetServiceDatabase/Tables/Gadget.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Gadgets](
+	[Id] [int] NOT NULL IDENTITY(1,1),
+	[Name] [nchar](10) NULL
+	CONSTRAINT PK_GadgetId PRIMARY KEY CLUSTERED(Id ASC)
+);
